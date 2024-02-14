Fix pawn controlled square calculation vs. movable square

src/board/board.py
@@ -129,7 +129,7 @@ class Position:
         color_pieces = self.position[self.position != None]
         color_pieces = np.array([piece for piece in color_pieces if piece.color == color])
 
-        candidate_moves = [[Move(piece.square, to_square) for to_square in piece.get_controlled_squares()] for piece in color_pieces]
+        candidate_moves = [[Move(piece.square, to_square) for to_square in piece.get_movable_squares()] for piece in color_pieces]
         candidate_moves = flatten_moves(candidate_moves)
         legal_moves = [move for move in candidate_moves if self.is_legal(move)]
 
@@ -148,9 +148,9 @@ class Position:
             print("No piece on specified square")
             return False
         
-        # Check that the piece that is moved actually controls (can move to) the specified square.
-        controlled_squares = np.array(moved_piece.get_controlled_squares())
-        if move.to_square.tolist() not in controlled_squares.tolist():
+        # Check that the piece that is moved can move to the specified square.
+        movable_squares = np.array(moved_piece.get_movable_squares())
+        if move.to_square.tolist() not in movable_squares.tolist():
             print("Not a controlled square")
             return False
         

src/board/piece/pawn.py
@@ -1,3 +1,4 @@
+from numpy.core.multiarray import array as array
 from . import Piece, Color, Move
 import numpy as np
 
@@ -5,15 +6,19 @@ class Pawn(Piece):
 
     def __init__(self, square: np.array, color: Color):
         super().__init__(square, color, 1)
+        self.movable_squares = []
 
-        # Assume that has moved if not on the second or 7th rank
+        # Has to have moved if not on the 2nd or 7th rank
         if square is not None and square[0] != 1 and square[0] != 6:
             self.nr_moves = 1
 
+    def get_movable_squares(self) -> np.array:
+        return self.movable_squares
+
     def calculate_controlled_squares(self, board: np.array):
-        # TODO think this through because technically the pawn doesn't
-        # control the squares in front of it
-        self.controlled_squares = np.vstack((self.get_advances(board), self.get_captures(board)))    
+        self.controlled_squares = self.get_capture_squares(board)
+        self.movable_squares = np.vstack(\
+            (self.get_advances(board), self.get_possible_captures(board, self.controlled_squares)))
 
     def get_advances(self, board: np.array) -> np.array:
         possible_squares = np.empty((0,2), dtype=int)
@@ -37,33 +42,36 @@ class Pawn(Piece):
 
         return possible_squares
     
-    def get_captures(self, board: np.array) -> np.array:
-        possible_squares = np.empty((0,2), dtype=int)
+    def get_capture_squares(self, board: np.array) -> np.array:
+        capture_squares = np.empty((0,2), dtype=int)
         # If white, move up the board, down otherwise
         direction = 1 if self.color == Color.White else -1
 
         capture_1 = np.copy(self.square)
         capture_1 += (direction, 1)
         if self.is_on_board(capture_1):
-            piece_to_capture = board[tuple(capture_1)]
-            if piece_to_capture is not None and \
-                piece_to_capture.color != self.color:
-
-                possible_squares = np.vstack((possible_squares, capture_1))
-                piece_to_capture.attacked_by.append(self)
+            capture_squares = np.vstack((capture_squares, capture_1))
 
         capture_2 = np.copy(self.square)
         capture_2 += (direction, -1)
         if self.is_on_board(capture_2):
-            piece_to_capture = board[tuple(capture_2)]
-            if piece_to_capture is not None and \
+            capture_squares = np.vstack((capture_squares, capture_2))
+
+        return capture_squares
+    
+    def get_possible_captures(self, board: np.array, capture_squares: np.array) -> np.array:
+        possible_captures = np.empty((0,2), dtype=int)
+        for sqr in capture_squares:
+            piece_to_capture = board[tuple(sqr)]
+            if piece_to_capture is not None and\
                 piece_to_capture.color != self.color:
 
-                possible_squares = np.vstack((possible_squares, capture_2))
-                piece_to_capture.attacked_by.append(self)
+                possible_captures = np.vstack((possible_captures, sqr))
+                piece_to_capture.attacked_by.append(self)            
 
-        return possible_squares
-    
+        return possible_captures
+
+
     def fen_symbol(self):
         if self.color == Color.White:
             return "P"

src/board/piece/piece.py
@@ -32,13 +32,22 @@ class Piece:
 
     def calculate_controlled_squares(self, board: np.array):
         """ Finds the squares the piece controls in the given position
-        and stores them in self.controlled_squares
+        and stores them in self.controlled_squares. Also recalculates
+        the movable squares in case those are different from the controlled
+        squares (pawn).
         """
         pass
 
     def get_controlled_squares(self) -> np.array:
-        """ Returns the possible moves for this piece in the given position
+        """ Returns the squares this piece controls in the given position
         or None if no controlled squares
+        """
+        return self.controlled_squares
+    
+    def get_movable_squares(self) -> np.array:
+        """ Returns the squares where the piece can move.
+        By default the same as controlled squares. The only exception
+        is the pawn who doesn't control the squares in front of it.
         """
         return self.controlled_squares
 

src/tests/piece_test.py
@@ -8,7 +8,7 @@ class TestPieces(unittest.TestCase):
     def setUp(self):
         self.board = Board()
 
-    def test_king_controlled_squares(self):
+    def test_king_controlled_and_movable_squares(self):
         # Kings should control 5 squares when game starts
         self.assertEqual(len(self.board.position.white_king.get_controlled_squares()), 5)
         self.assertEqual(len(self.board.position.black_king.get_controlled_squares()), 5)
@@ -17,7 +17,12 @@ class TestPieces(unittest.TestCase):
         self.assertEqual(len(self.board.position.white_king.get_controlled_squares()), 8)
         self.assertEqual(len(self.board.position.black_king.get_controlled_squares()), 8)
 
-    def test_knight_controlled_squares(self):
+        self.assertTrue((self.board.position.white_king.get_controlled_squares()\
+                          == self.board.position.white_king.get_movable_squares()).all())
+        self.assertTrue((self.board.position.black_king.get_controlled_squares()\
+                          == self.board.position.black_king.get_movable_squares()).all())
+
+    def test_knight_controlled_and_movable_squares(self):
         white_knight_1 = self.board.position[0, 1]
         white_knight_2 = self.board.position[0, 6]
         black_knight_1 = self.board.position[7, 1]
@@ -33,7 +38,10 @@ class TestPieces(unittest.TestCase):
         self.assertEqual(len(white_knight.get_controlled_squares()), 8)
         self.assertEqual(len(black_knight.get_controlled_squares()), 6)
 
-    def test_bishop_controlled_squares(self):
+        self.assertTrue((white_knight.get_controlled_squares() == white_knight.get_movable_squares()).all())
+        self.assertTrue((black_knight.get_controlled_squares() == black_knight.get_movable_squares()).all())
+
+    def test_bishop_controlled_and_movable_squares(self):
         wb1 = self.board.position[0,2]
         wb2 = self.board.position[0,5]
         bb1 = self.board.position[7,2]
@@ -49,7 +57,10 @@ class TestPieces(unittest.TestCase):
         self.assertEqual(len(wb.get_controlled_squares()), 8)
         self.assertEqual(len(bb.get_controlled_squares()), 13)
 
-    def test_queen_controlled_squares(self):
+        self.assertTrue((wb.get_controlled_squares() == wb.get_movable_squares()).all())
+        self.assertTrue((bb.get_controlled_squares() == bb.get_movable_squares()).all())
+
+    def test_queen_controlled_and_movable_squares(self):
         wq = self.board.position[0,3]
         bq = self.board.position[7,3]
         self.assertEqual(len(wq.get_controlled_squares()), 5)
@@ -60,8 +71,11 @@ class TestPieces(unittest.TestCase):
         bq = self.board.position[4, 4]
         self.assertEqual(len(wq.get_controlled_squares()), 19)
         self.assertEqual(len(bq.get_controlled_squares()), 20)
+
+        self.assertTrue((wq.get_controlled_squares() == wq.get_movable_squares()).all())
+        self.assertTrue((bq.get_controlled_squares() == bq.get_movable_squares()).all())
         
-    def test_rook_controlled_squares(self):
+    def test_rook_controlled_and_movable_squares(self):
         wr1 = self.board.position[0,0]
         wr2 = self.board.position[0,7]
         br1 = self.board.position[7,0]
@@ -77,10 +91,17 @@ class TestPieces(unittest.TestCase):
         self.assertEqual(len(wr.get_controlled_squares()), 14)
         self.assertEqual(len(br.get_controlled_squares()), 11)
 
-    def test_pawn_controlled_squares(self):
+        self.assertTrue((wr.get_controlled_squares() == wr.get_movable_squares()).all())
+        self.assertTrue((br.get_controlled_squares() == br.get_movable_squares()).all())
+
+    def test_pawn_controlled_and_movable_squares(self):
         for (_, _), piece in np.ndenumerate(self.board.position.position):
             if type(piece) == Pawn:
-                # TODO: Currently considers the squares in front of the pawn controlled as well which will have to be fixed
+                if piece.square[1] == 0 or piece.square[1] == 7:
+                    # Corner pawn
+                    self.assertEqual(len(piece.get_controlled_squares()), 1)
+                    continue
+
                 self.assertEqual(len(piece.get_controlled_squares()), 2)
 
         self.board.load_board_fen("8/4p3/3k1P2/4N3/3K4/8/8/8")
@@ -90,6 +111,7 @@ class TestPieces(unittest.TestCase):
         wp.calculate_controlled_squares(self.board.position)
         bp.nr_moves = 0
         bp.calculate_controlled_squares(self.board.position)
-        print(wp.get_controlled_squares())
         self.assertEqual(len(wp.get_controlled_squares()), 2)
         self.assertEqual(len(bp.get_controlled_squares()), 2)
+        self.assertEqual(len(wp.get_movable_squares()), 2) # Forward and capture
+        self.assertEqual(len(bp.get_movable_squares()), 2) # Forward and capture